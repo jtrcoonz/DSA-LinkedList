2c17b24251357e06d428dfcad38628847680f147
Jacob Coons <jacobtcoons@gmail.com>
2019-09-06 12:53:33 -0700

    modularized classes

--- linked-list-class.js
+++ linked-list-class.js
@@ -1,10 +1,3 @@
-class _Node {
-    constructor(value, next) {
-        this.value = value;
-        this.next = next;
-    }
-}
-
 class LinkedList {
     constructor() {
         this.head = null;
@@ -25,4 +18,42 @@ class LinkedList {
             tempNode.next = new _Node(item, null);
         }
     }
+    find(item) { 
+        let currNode = this.head;
+        if (!this.head) {
+            return null;
+        }
+        while (currNode.value !== item) {
+            if (currNode.next === null) {
+                return null;
+            }
+            else {
+                currNode = currNode.next;
+            }
+        }
+        return currNode;
+    }
+    remove(item) { 
+        if (!this.head) {
+            return null;
+        }
+        if (this.head.value === item) {
+            this.head = this.head.next;
+            return;
+        }
+        let currNode = this.head;
+        let previousNode = this.head;
+
+        while ((currNode !== null) && (currNode.value !== item)) {
+            previousNode = currNode;
+            currNode = currNode.next;
+        }
+        if (currNode === null) {
+            console.log('Item not found');
+            return;
+        }
+        previousNode.next = currNode.next;
+    }
 }
+
+module.exports = _Node
